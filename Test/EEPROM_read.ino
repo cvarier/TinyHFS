ac01f9fdc4c89bca0cd196d620590aaba403c6db
Chaitanya Varier <cvvarier@gmail.com>
2016-11-19 00:55:57 -0500

    Added working code for writing and reading strings to EEPROM

---
+++ Test/EEPROM_read.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#define eeprom 0x54    //Address of 24LC256 eeprom chip
+#define str_len 500
+#define startAddress 0
+
+unsigned char str_bytes[str_len];
+static TwoWire orbitBoosterWire(0);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  orbitBoosterWire.begin();
+
+  Serial.println("DATA READ");
+
+  // Read the data byte-by-byte and store in string str_bytes
+  for(int i = startAddress; i < startAddress + str_len; i++)
+    str_bytes[i-startAddress] = readByte(eeprom, i);
+
+  // Print out the read data
+  for(int i = 0; i < str_len; i++)
+    Serial.print((char) str_bytes[i]);
+
+  Serial.println();
+
+}
+ 
+void loop(){
+}
+ 
+char readByte(int deviceaddress, int eeaddress) 
+{
+  char rdata = 0xFF;
+  
+  orbitBoosterWire.beginTransmission(deviceaddress);
+  orbitBoosterWire.write((int)(eeaddress >> 8));   // MSB
+  orbitBoosterWire.write((int)(eeaddress & 0xFF)); // LSB
+  orbitBoosterWire.endTransmission();
+ 
+  orbitBoosterWire.requestFrom(deviceaddress, 1);
+ 
+  if(orbitBoosterWire.available()) rdata = orbitBoosterWire.read();
+
+  return rdata;
+
+}
---
+++ Test/EEPROM_write.ino
@@ -0,0 +1,66 @@
+#include <Wire.h>
+#include <string.h>
+#define eeprom 0x54    //Address of 24LC256 eeprom chip
+#define startAddress 0
+
+static TwoWire orbitBoosterWire(0);
+String inData;
+int strRcvd = 0;
+int str_len = 0;
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  orbitBoosterWire.begin();
+  Serial.println("Enter a string...");
+}
+
+void loop() {
+  
+    while (Serial.available() > 0 && !strRcvd) {
+        
+        char recieved = Serial.read();
+        inData += recieved; 
+
+        // Process string when new line character is recieved
+        if (recieved == '\n')
+        {
+          Serial.print("String received: ");
+          strRcvd = 1;
+  
+          // Work out length of data
+          int str_len = inData.length() - 1;
+          char str_bytes[str_len];
+        
+          // Split data into an array of char arrays, each one byte long
+          for(int j = 0; j < str_len; j++)
+            str_bytes[j] = inData[j];
+        
+          Serial.println();
+
+          // Print processed string
+          for(int q = 0; q < str_len; q++)
+            Serial.print(str_bytes[q]);
+          
+          // Write data to EEPROM
+          for(int i = startAddress; i < startAddress + str_len; i++) writeByte(eeprom, i, str_bytes[i-startAddress]);
+        
+          Serial.print("\nDATA WRITTEN - length: ");
+          Serial.print(str_len);
+          Serial.println();
+  
+        }
+    }
+}
+
+void writeByte(int deviceaddress, int eeaddress, char data ) 
+{
+  orbitBoosterWire.beginTransmission(deviceaddress);
+  orbitBoosterWire.write((int)(eeaddress >> 8));   // MSB
+  orbitBoosterWire.write((int)(eeaddress & 0xFF)); // LSB
+  orbitBoosterWire.write(data);
+  orbitBoosterWire.endTransmission();
+ 
+  delay(5);
+}
